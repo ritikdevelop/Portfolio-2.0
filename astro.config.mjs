02556bf2f35ab18105cd2868b4b03d8f2e2883d5
ritikdevelop <hrithikrana216@gmail.com>
2025-10-23 12:42:09 +0530

    feat: update dependencies and add new packages for enhanced functionality
    - Updated package.json to include new dependencies such as @astrojs/markdown-remark, @astrojs/mdx, and others for improved project capabilities.
    - Added a new PDF resume for Ritik Rana to the public directory.
    - Created a new Tailwind CSS configuration file to customize styles and integrate daisyUI and scrollbar plugins.
    - Enhanced TypeScript configuration with baseUrl and paths for better module resolution.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// @ts-nocheck
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import astroIcon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import playformCompress from "@playform/compress";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    astroIcon({
+      include: {
+        mdi: ["*"],
+        ri: ["*"],
+        "simple-icons": ["*"],
+      },
+    }),
+    playformCompress({
+      CSS: false,
+      Image: false,
+      Action: {
+        Passed: async () => true,
+      },
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
+  devToolbar: {
+    enabled: false,
+  },
+});
